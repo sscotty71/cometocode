3f73f2922583ab649817bc1e9c5ef95df72d358a
klonet <stefano.denardis@klonet.it>
2024-09-18 17:01:16 +0200

    milgiorato il codice di terraform + modificata la documentazione

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,30 +1,38 @@
-resource "proxmox_vm_qemu" "cloudinit-test" {
-    # name = "terraform-test-vm"
-    # desc = "A test for using terraform and cloudinit"
-    for_each = { for vm in var.VM : vm.name => vm }
-     name = each.value.name
-     vmid = each.value.vmid
+# Leggi il file YAML
+locals {
+  vms = yamldecode(file("vm_info.yaml"))["vms"]
+  validate_vms = length(local.vms) > 0 ? true : error("L'inventario JSON è vuoto. Nessuna VM da creare.")
+}
 
+# locals {
+#   users = yamldecode(file("users.yaml"))["users"]
+#   validate_users = length(local.users) > 0 ? true : error("L'inventario users JSON è vuoto. Nessuna VM da creare.")
+# }
+
+
+resource "proxmox_vm_qemu" "cloudinit-test" {
+    count = length(local.vms)
+     
+     vmid = local.vms[count.index]["vmid"]
+     name = local.vms[count.index]["name"]
+      
      target_node = var.TARGET_NODE
-    //desc = "A test for using terraform and cloudinit"
  
-   # clone = "lookup(var.TEMPLATE, "ubuntu")"
-    clone = "ubuntu-noble"
+     clone = local.vms[count.index]["image"]
     # Activate QEMU agent for this VM
-     agent = 0
+     agent = 1
 
     os_type = "cloud-init"
+
+    cores  = local.vms[count.index]["cpu"]
+    memory = local.vms[count.index]["memory"]
+
+    pool = "come-to-code"
    
-    cores = 2
-    sockets = 1
-    vcpus = 0
-   cpu         = "host"
-  memory      =  1024
 
     vm_state    = "started"
-    #scsihw = "lsi" 
     scsihw      = "virtio-scsi-single"
-    # Setup the disk
+    
     disks {
         ide {
             ide3 {
@@ -36,7 +44,7 @@ resource "proxmox_vm_qemu" "cloudinit-test" {
          scsi {
            scsi0 {
             disk {
-                    size            =  each.value.disk_size
+                    size            =  local.vms[count.index]["disk_size"]
                     cache           = "writeback"
                     storage         = "local-lvm"
              
@@ -57,13 +65,14 @@ resource "proxmox_vm_qemu" "cloudinit-test" {
       boot        = "order=scsi0;net0"
 
     # Keep in mind to use the CIDR notation for the ip.
-     ciuser = each.value.user
-     nameserver = each.value.nameserver
-     ipconfig0 = each.value.ipconfig0
+     
+     nameserver = local.vms[count.index]["nameserver"]
+     ipconfig0 = local.vms[count.index]["ipconfig0"]
+
+     ciuser = local.vms[count.index]["user"]
      sshkeys = <<EOF
       ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDA20F2OR+1k3G9uqAPVK6jocAmRVwP0ZjeyEpTaqOSBBkzu/hl8HtenWy4DlZ9HZlgTMnzgzkKRuTvLvjRzdWJWFDrcJOYD8Af/9DZiy3eu8wc5For6q0i4JD600PqI2hBC0Kp2f8rTRAW/dsbtM7I90BjZhDDUHpbup/OwvKnEj/edyXUcsV3B64IXXqF//XQYmyKajf8Sc3Zhq8ttEUqSGjDfB9wO+uhtHdEuaFCh4mw3F+dLaBWL6nFP5vW6+UIuQ2AGJ/01xRKv7LRidAms339dbaDvnbID8SY87zaAZzIeNreO2G9aJqSJNZeveRjX/7gA/WuBw2FM4WCraKj stefano.denardis@MacBook-Air-di-Stefano.local
      EOF
- # Aggiungi altre configurazioni della VM...
 
    connection {
        type     = "ssh"
@@ -89,7 +98,7 @@ resource "local_file" "ansible_inventory" {
   filename = "../inventory.yml"
   content = <<EOF
 
-${templatefile("../inventory.tpl", { vm = var.VM })}
+${templatefile("../inventory.tpl", { vm = local.vms })}
 EOF
 }
 
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -12,100 +12,132 @@
 #     error_message = "L'input deve essere un indirizzo IPv4 valido o un hostname valido."
 #   }
 #  }
+variable "VAULT_TOKEN" {
+  description = "Vault token for authentication"
+  type        = string
+  sensitive   = true
+}
 
 
+ variable "sshkeys" {
+    type = string
+    default = ""
+    
+ }
+  variable "user" {
+    type = string
+    default = ""
+    
+ }
+ 
  variable "TARGET_NODE" {
     type = string
     default = "pve"
     
  }
-    
-variable "VM" {
-  description = "Lista delle configurazioni delle VM"
-  type = list(object({
-     vmid  = number
-    name   = string
-    cpu    = number
-    memory = number
-    user   = string
-    nameserver = string
-    image  = string
-    disk_size = number
-    ipconfig0   = string
+
+# variable "vms" {
+#   type = list(object({
+#     name       = string
+#     vmid       = number
+#     cpu        = number
+#     memory     = number
+#     image      = string
+#     disk_size  = number
+#     user       = string
+#     nameserver = string
+#     sshkeys    = string
+#     ipconfig0  = string
+#   }))
+# }
+
+
+#  } 
+# variable "VM" {
+#   description = "Lista delle configurazioni delle VM"
+#   type = list(object({
+#      vmid  = number
+#     name   = string
+#     cpu    = number
+#     memory = number
+#     user   = string
+#     nameserver = string
+#     image  = string
+#     disk_size = number
+#     ipconfig0   = string
     
   
-  }))
-  default = [
-    {
-      vmid    = 120
-      name    = "pgprimary"
-      cpu     = 1
-      memory  = 1024
-      image   = "ubuntu-noble"
-      user    = "stefano"
-      nameserver = "8.8.8.8"
-      disk_size = 25
-      ipconfig0 = "ip=192.168.173.25/24,gw=192.168.173.254"
+#   }))
+#   default = [
+#     {
+#       vmid    = 120
+#       name    = "pgprimary"
+#       cpu     = 1
+#       memory  = 1024
+#       image   = "ubuntu-noble"
+#       user    = "stefano"
+#       nameserver = "8.8.8.8"
+#       disk_size = 25
+#       ipconfig0 = "ip=192.168.173.25/24,gw=192.168.173.254"
       
    
-      },
-      {
-        vmid   = 121
-        name   = "pgsecondary"
-        cpu    = 1
-        memory = 1024
-        image  = "ubuntu"
-        user    = "stefano"
-        nameserver = "8.8.8.8"
-        disk_size = 25
-        ipconfig0 = "ip=192.168.173.26/24,gw=192.168.173.254"
-      },
-        {
-         vmid   = 122
-         name   = "pgbackrest"
-         cpu    = 1
-         memory = 1024
-         image  = "ubuntu-noble"
-         user    = "stefano"
-         nameserver = "8.8.8.8"
-         disk_size = 25
-         ipconfig0 = "ip=192.168.173.27/24,gw=192.168.173.254"
-       },
-        {
-         vmid   = 123
-         name   = "bucketserver"
-         cpu    = 1
-         memory = 1024
-         image  = "ubuntu-noble"
-         user    = "stefano"
-         nameserver = "8.8.8.8"
-         disk_size = 25
-         ipconfig0 = "ip=192.168.173.28/24,gw=192.168.173.254"
-       },
-        {
-         vmid   = 124
-         name   = "monitor"
-         cpu    = 1
-         memory = 1024
-         image  = "ubuntu-noble"
-         user    = "stefano"
-         nameserver = "8.8.8.8"
-         disk_size = 25
-         ipconfig0 = "ip=192.168.173.29/24,gw=192.168.173.254"
-       }
-  ]
-}
+#       },
+#       {
+#         vmid   = 121
+#         name   = "pgsecondary"
+#         cpu    = 1
+#         memory = 1024
+#         image  = "ubuntu"
+#         user    = "stefano"
+#         nameserver = "8.8.8.8"
+#         disk_size = 25
+#         ipconfig0 = "ip=192.168.173.26/24,gw=192.168.173.254"
+#       },
+#         {
+#          vmid   = 122
+#          name   = "pgbackrest"
+#          cpu    = 1
+#          memory = 1024
+#          image  = "ubuntu-noble"
+#          user    = "stefano"
+#          nameserver = "8.8.8.8"
+#          disk_size = 25
+#          ipconfig0 = "ip=192.168.173.27/24,gw=192.168.173.254"
+#        },
+#         {
+#          vmid   = 123
+#          name   = "bucketserver"
+#          cpu    = 1
+#          memory = 1024
+#          image  = "ubuntu-noble"
+#          user    = "stefano"
+#          nameserver = "8.8.8.8"
+#          disk_size = 25
+#          ipconfig0 = "ip=192.168.173.28/24,gw=192.168.173.254"
+#        },
+#         {
+#          vmid   = 124
+#          name   = "monitor"
+#          cpu    = 1
+#          memory = 1024
+#          image  = "ubuntu-noble"
+#          user    = "stefano"
+#          nameserver = "8.8.8.8"
+#          disk_size = 25
+#          ipconfig0 = "ip=192.168.173.29/24,gw=192.168.173.254"
+#        }
+#   ]
+# }
 
 
 
-#  variable "PROXMOX_PASSWORD" {}
-#  variable "PROXMOX_USERNAME" {}
+# #  variable "PROXMOX_PASSWORD" {}
+# #  variable "PROXMOX_USERNAME" {}
 
  variable "OS" {
     type = string
     default = "ubuntu"
  }
-
 
  variable "TEMPLATE" {
     type = map
